c1cc1b8f6188c8c38ae5fcafe43371e06da3f45d
Akshit Aryan <aryanakshit152@gmail.com>
2024-02-12 23:39:26 +0530

    first commit

---
+++ src/app/components/Header.jsx
@@ -0,0 +1,36 @@
+"use client";
+import React from "react";
+
+const Header = () => {
+  return (
+    <div className="flex items-center justify-between w-full px-5 py-5 bg-green-300 shadow-xl shadow-green-200">
+      <div className="font-extrabold text-[22px] cursor-pointer">
+        PreferRefer
+      </div>
+      <div className="flex items-center space-x-6">
+        <span className="font-medium transition-all cursor-pointer hover:underline">
+          Home
+        </span>
+        <span className="font-medium transition-all cursor-pointer hover:underline">
+          Browse Referrals
+        </span>
+        <span className="font-medium transition-all cursor-pointer hover:underline">
+          Post Referral
+        </span>
+      </div>
+      <div className="flex items-center space-x-6">
+        <span className="font-medium transition-all cursor-pointer hover:underline">
+          Login
+        </span>
+        <span className="font-medium transition-all cursor-pointer hover:underline">
+          Register
+        </span>
+        <span className="font-medium transition-all cursor-pointer hover:underline">
+          Logout
+        </span>
+      </div>
+    </div>
+  );
+};
+
+export default Header;
